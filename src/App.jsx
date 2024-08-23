f3f7a0c8d385979aebd89768e3a70ef4cf19383c
Gaurav Antal <68815799+GauravAntal15@users.noreply.github.com>
2024-08-23 21:13:45 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Navbar from "./components/Navbar"
 import * as Sentry from '@sentry/react';
 
 const App = () => {
+  return <button onClick={() => methodDoesNotExist()}
+  >Break the world</button>;
   return (
     <main className="bg-black">
 
